1dc1349e26ee7d7f62cf25a835d8fa827470f04d
Smile of a Mac <Smile-of-a-Mac@users.noreply.github.com>
2026-07-03 17:20:28 +0800

    更新项目配置和构建脚本

--- build_app.sh
+++ build_app.sh
@@ -34,16 +34,16 @@ mkdir -p "${APP}/Contents/Resources"
 cp "${BINARY}" "${APP}/Contents/MacOS/Ruffnova"
 
 # Copy Info.plist
-cp "${PROJECT_DIR}/Ruffnova/Info.plist" "${APP}/Contents/Info.plist"
+cp "${PROJECT_DIR}/Info.plist" "${APP}/Contents/Info.plist"
 
 # Copy asset catalog
-if [[ -d "${PROJECT_DIR}/Ruffnova/Assets.xcassets" ]]; then
-    cp -R "${PROJECT_DIR}/Ruffnova/Assets.xcassets" "${APP}/Contents/Resources/Assets.xcassets"
+if [[ -d "${PROJECT_DIR}/Assets.xcassets" ]]; then
+    cp -R "${PROJECT_DIR}/Assets.xcassets" "${APP}/Contents/Resources/Assets.xcassets"
 fi
 
 # Copy any additional resources
-if [[ -d "${PROJECT_DIR}/Ruffnova/Resources" ]]; then
-    cp -R "${PROJECT_DIR}/Ruffnova/Resources" "${APP}/Contents/Resources/"
+if [[ -d "${PROJECT_DIR}/Resources" ]]; then
+    cp -R "${PROJECT_DIR}/Resources" "${APP}/Contents/Resources/"
 fi
 
 # Generate PkgInfo
